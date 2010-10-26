bbc0138cfc937423a8d09d2722b21a95e9d3f5f1
Urban Suppiger <u.suppiger@mybytes.ch>
2010-10-26 13:40:39 +0200

    empty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scoutdb::Application.config.secret_token = '1b038e3ffc682bffc8514cbab1fc0a4774a56fb0bab4062334b143de929526b0d3a42245db02d6b46611dcfa8419310b63a7ff3f35f462ed2ccd7cc367ca4138'
